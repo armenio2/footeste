a1ecd0815ec6544964cec54fde609e3b0a669153
Armenio <dehme.armenio@gmail.com>
2024-03-06 09:57:33 +0000

    f

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require('express');
 const app = express();
 
 // Rota para "Hello, World!"
+app.get('/hello', (req, res) => {
+    res.send('Hello, World!');
+});
+
 app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/foo2', (req, res) => {
     res.send('Hello, World!');
 });
 
--- public/index.js
+++ public/index.js
@@ -7,4 +7,12 @@ app.get('/hello', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/foo', (req, res) => {
+    res.send('Hello, World!');
+});
+
 exports.app = functions.https.onRequest(app);
